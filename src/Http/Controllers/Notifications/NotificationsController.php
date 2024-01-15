deb07566cff149e949684bb08ab672a9b373be0a
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-01-15 19:29:23 +0300

    Abstract controller fixed

--- src/Http/Controllers/Notifications/NotificationsController.php
+++ src/Http/Controllers/Notifications/NotificationsController.php
@@ -4,7 +4,7 @@ namespace NextDeveloper\Communication\Http\Controllers\Notifications;
 
 use Illuminate\Http\Request;
 use NextDeveloper\Communication\Http\Controllers\AbstractController;
-use NextDeveloper\Generator\Http\Traits\ResponsableFactory;
+use NextDeveloper\Commons\Http\Response\ResponsableFactory;
 use NextDeveloper\Communication\Http\Requests\Notifications\NotificationsUpdateRequest;
 use NextDeveloper\Communication\Database\Filters\NotificationsQueryFilter;
 use NextDeveloper\Communication\Database\Models\Notifications;
